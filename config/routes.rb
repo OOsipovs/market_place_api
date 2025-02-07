108b1c2d8761a13c8950b9c93cd92f5f72bda44a
Olegs Osipovs <olegs.osipovs@gmail.com>
2025-02-07 15:38:57 +0200

    Versioning added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # API definition
   namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+    end
   end
 
   # get "up" => "rails/health#show", as: :rails_health_check
