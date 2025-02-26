993812ec8dfca7f16d9f25734042b74bddbb31cb
Olegs Osipovs <olegs.osipovs@gmail.com>
2025-02-26 22:32:11 +0200

    Products serialization added

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::ProductsController < ApplicationController
     def create
         product = current_user.products.build(product_params)
         if product.save
-            render json: product, status: :created
+            render json: ProductSerializer.new(product).serializable_hash.to_json, status: :created
         else
             render json: { errors: product.errors }, status: :unprocessable_entity
         end
@@ -14,7 +14,7 @@ class Api::V1::ProductsController < ApplicationController
 
     def update
         if @product.update(product_params)
-            render json: @product
+            render json: ProductSerializer.new(@product).serializable_hash.to_json
         else
             render json: @product.errors, status: :unprocessable_entity
         end
@@ -26,11 +26,13 @@ class Api::V1::ProductsController < ApplicationController
     end
 
     def index
-        render json: Product.all
+        @products = Product.all
+        render json: ProductSerializer.new(@products).serializable_hash.to_json
     end
 
     def show
-        render json: Product.find(params[:id])
+        @product = Product.find(params[:id])
+        render json: ProductSerializer.new(@product).serializable_hash.to_json
     end
 
     private
